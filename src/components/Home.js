a324c6921086423ee77db22eda1627ac57a1f57a
Tim Paine <t.paine154@gmail.com>
2019-04-25 17:07:26 -0400

    working on categories and pagination

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,13 +12,20 @@ class Home extends Component {
     render(){
         let itemList = this.props.items.map(item=>{
             return(
-                <div className="card" key={item.id}>
+                <div className="card small" key={item.id}>
                     <div className="card-image">
-                            <img src={item.img} alt={item.title}/>
-                        <span className="card-title teal">{item.title}</span>
-                        <span to="/" className="btn-floating halfway-fab waves-effect waves-light" 
-                            onClick={()=>{this.handleClick(item.id)}}>
+                        <img src={item.img} alt={item.title}/>
+                        <span to="/" className="btn-floating waves-effect waves-light" 
+                            onClick={()=>{this.handleClick(item.id)}}
+                            style={{position:"absolute", right:24, top:5}}>
                             <i className="material-icons">add</i>
+                        </span>
+                        <span className="card-title teal left"
+                            style={{position:"absolute", right:0,
+                                    top:0, height: 50,
+                                    padding:5
+                                    }}>
+                            {item.title}
                         </span>
                     </div>
 
@@ -27,7 +34,6 @@ class Home extends Component {
                         <p><b>Price: {item.price}$</b></p>
                     </div>
                  </div>
-
             )
         })
 
@@ -41,10 +47,16 @@ class Home extends Component {
                         </div>
                     </nav>
                     <ul className="collection">
-                        <a className="collection-item">Category 1</a>
-                        <a className="collection-item">Category 2</a>
-                        <a className="collection-item">Category 3</a>
-                        <a className="collection-item">Category 4</a>
+                        <a href="/shop/category1" className="collection-item">Category 1</a>
+                        <a href="/shop/category2" className="collection-item">Category 2</a>
+                        <a href="/shop/category3" className="collection-item">Category 3</a>
+                        <a href="/shop/category4" className="collection-item">Category 4</a>
+                        <a href="/shop/category5" className="collection-item">Category 5</a>
+                        <a href="/shop/category6" className="collection-item">Category 6</a>
+                        <a href="/shop/category7" className="collection-item">Category 7</a>
+                        <a href="/shop/category8" className="collection-item">Category 8</a>
+                        <a href="/shop/category9" className="collection-item">Category 9</a>
+                        <a href="/shop/category10" className="collection-item">Category 10</a>
                     </ul>
                 </div>
                 <div className="col s8"
@@ -58,13 +70,13 @@ class Home extends Component {
                 <div className="row s1">
                     <div className="col s12 center">
                     <ul class="pagination">
-                        <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
-                        <li class="active"><a href="#!">1</a></li>
-                        <li class="waves-effect"><a href="#!">2</a></li>
-                        <li class="waves-effect"><a href="#!">3</a></li>
-                        <li class="waves-effect"><a href="#!">4</a></li>
-                        <li class="waves-effect"><a href="#!">5</a></li>
-                        <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+                        <li class="disabled"><a href="/shop"><i class="material-icons">chevron_left</i></a></li>
+                        <li class="teal active"><a href="/shop/browse/1">1</a></li>
+                        <li class="waves-effect"><a href="/shop/browse/2">2</a></li>
+                        <li class="waves-effect"><a href="/shop/browse/2">3</a></li>
+                        <li class="waves-effect"><a href="/shop/browse/2">4</a></li>
+                        <li class="waves-effect"><a href="/shop/browse/2">5</a></li>
+                        <li class="waves-effect"><a href="/shop/browse/2"><i class="material-icons">chevron_right</i></a></li>
                     </ul>
                     </div>
                 </div>
