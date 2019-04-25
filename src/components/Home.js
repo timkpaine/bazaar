fb9bf3a7bc45ac50e9dedb261a1c9fd5d0567bdc
Tim Paine <t.paine154@gmail.com>
2019-04-25 16:48:32 -0400

    add browser and pagination

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,27 +13,62 @@ class Home extends Component {
         let itemList = this.props.items.map(item=>{
             return(
                 <div className="card" key={item.id}>
-                        <div className="card-image">
+                    <div className="card-image">
                             <img src={item.img} alt={item.title}/>
-                            <span className="card-title">{item.title}</span>
-                            <span to="/" className="btn-floating halfway-fab waves-effect waves-light" onClick={()=>{this.handleClick(item.id)}}><i className="material-icons">add</i></span>
-                        </div>
+                        <span className="card-title teal">{item.title}</span>
+                        <span to="/" className="btn-floating halfway-fab waves-effect waves-light" 
+                            onClick={()=>{this.handleClick(item.id)}}>
+                            <i className="material-icons">add</i>
+                        </span>
+                    </div>
 
-                        <div className="card-content">
-                            <p>{item.desc}</p>
-                            <p><b>Price: {item.price}$</b></p>
-                        </div>
+                    <div className="card-content small">
+                        <p>{item.desc}</p>
+                        <p><b>Price: {item.price}$</b></p>
+                    </div>
                  </div>
 
             )
         })
 
         return(
-            <div className="container">
-                <h3 className=""> </h3>
-                <div className="box">
-                    {itemList}
+            <div className="row">
+                <div className="col s4"
+                    style={{paddingLeft: 0}}>
+                    <nav className="nav-wrapper blue">
+                        <div className="col s12">
+                            <a href="/" class="breadcrumb">Browse</a>
+                        </div>
+                    </nav>
+                    <ul className="collection">
+                        <a className="collection-item">Category 1</a>
+                        <a className="collection-item">Category 2</a>
+                        <a className="collection-item">Category 3</a>
+                        <a className="collection-item">Category 4</a>
+                    </ul>
                 </div>
+                <div className="col s8"
+                    style={{paddingLeft: 0, paddingRight: 0}}>
+                <div className="col s12"
+                    style={{paddingLeft: 0}}>
+                    <div className="box">
+                        {itemList}
+                    </div>
+                </div>
+                <div className="row s1">
+                    <div className="col s12 center">
+                    <ul class="pagination">
+                        <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+                        <li class="active"><a href="#!">1</a></li>
+                        <li class="waves-effect"><a href="#!">2</a></li>
+                        <li class="waves-effect"><a href="#!">3</a></li>
+                        <li class="waves-effect"><a href="#!">4</a></li>
+                        <li class="waves-effect"><a href="#!">5</a></li>
+                        <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
             </div>
         )
     }
